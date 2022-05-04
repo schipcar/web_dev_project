f2469a54953bbd54c3d52ad84ca2616a828faa30
cschippits <cschippits@uchicago.edu>
2022-05-03 20:03:10 -0500

    Added assignment pages

--- canvas_functions.js
+++ canvas_functions.js
@@ -34,6 +34,89 @@ function add_announcement_form() {
     div.appendChild(text_box);  
     div.appendChild(linebreak);  
     div.appendChild(submit_button);
-    document.getElementById('announcements_panel').appendChild(div);
+    document.getElementById('main_panel').appendChild(div);
+ 
+}
+
+
+function add_assignment_form() {
+
+    div = document.createElement("div");
+    div.className = 'assignment_form';
+    div.id = 'assignment_form_div';
+
+    name_prompt = document.createElement("p");
+    duedate_prompt = document.createElement("p");
+    points_prompt = document.createElement("p");
+    description_prompt = document.createElement("p");
+
+    name_prompt.innerHTML = 'Assignment Name:';
+    duedate_prompt.innerHTML = 'Due Date:';
+    points_prompt.innerHTML = 'Point Total:';
+    description_prompt.innerHTML = 'Description:';
+
+    name_box = document.createElement("input");
+    duedate_box = document.createElement("input");
+    points_box = document.createElement("input");
+    description_box = document.createElement("textarea");
+
+    name_box.className = 'announcement_form';
+    duedate_box.className = 'announcement_form';
+    points_box.className = 'announcement_form';
+    description_box.className = 'announcement_form';
+
+    name_box.setAttribute("type", "text");
+    duedate_box.setAttribute("type", "text");
+    points_box.setAttribute("type", "text");
+    description_box.rows = '4';
+
+    submit_button = document.createElement("button");         
+    submit_button.innerHTML = 'Submit';
+    submit_button.addEventListener("click", function() {
+        document.getElementById('assignment_form_div').remove();
+    })
+
+    linebreak = document.createElement("br");
+
+    div.appendChild(name_prompt);
+    div.appendChild(name_box);  
+    div.appendChild(linebreak);  
+    div.appendChild(duedate_prompt);
+    div.appendChild(duedate_box);  
+    div.appendChild(linebreak);  
+    div.appendChild(points_prompt);
+    div.appendChild(points_box);  
+    div.appendChild(linebreak);  
+    div.appendChild(description_prompt);
+    div.appendChild(description_box);  
+    div.appendChild(linebreak);  
+    div.appendChild(submit_button);
+    document.getElementById('main_panel').appendChild(div);
+ 
+}
+
+
+function add_submission_form() {
+
+    div = document.createElement("div");
+    div.className = 'assignment_form';
+    div.id = 'assignment_form_div';
+
+    text_box = document.createElement("textarea");
+    text_box.className = 'assignment_form';
+    text_box.rows = '4';
+
+    submit_button = document.createElement("button");         
+    submit_button.innerHTML = 'Submit';
+    submit_button.addEventListener("click", function() {
+        document.getElementById('assignment_form_div').remove();
+    })
+
+    linebreak = document.createElement("br");
+
+    div.appendChild(text_box);  
+    div.appendChild(linebreak);  
+    div.appendChild(submit_button);
+    document.getElementById('main_panel').appendChild(div);
  
 }
