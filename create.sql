c27f6df626134e419b0d2d8400ce5985121c68bc
anasofiamvaladez <anasofiamvaladez@gmail.com>
2022-05-11 13:17:36 -0500

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE grades (id INTEGER PRIMARY KEY, name TEXT, status TEXT, punctuation INTEGER, possible INTEGER);
+CREATE TABLE grades (id INTEGER PRIMARY KEY, student TEXT, name TEXT, status TEXT, punctuation INTEGER, possible INTEGER);
 CREATE TABLE users (name TEXT, email TEXT, id INTEGER, role TEXT check(role='student' or role='teacher' or role='admin'), status TEXT check(status='active' or status='inactive'), password TEXT, sec_q1 TEXT, sec_q2 TEXT, sec_q3 TEXT);
 CREATE TABLE courses (name TEXT, teacher TEXT, description TEXT, capacity INTEGER);
 CREATE TABLE courses_students (course_name TEXT, user_id INTEGER);
