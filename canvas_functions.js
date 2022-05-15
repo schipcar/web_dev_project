af2fa590c407ae14f66d065888b0f3e70407a95e
Carly Schippits <77079862+schipcar@users.noreply.github.com>
2022-05-15 11:50:50 -0500

    Modified print statement

--- canvas_functions.js
+++ canvas_functions.js
@@ -369,7 +369,7 @@ function LoadAnnouncements() {
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
           let announcements = JSON.parse(this.responseText);
-          document.getElementById("main_panel").innerHTML += String(anouncements.length)
+          document.getElementById("main_panel").innerHTML += anouncements
           for (let i=0; i<announcements.length; i++) {
               let announcement = announcements[i]
              
