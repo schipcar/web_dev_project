ec2696b9ef3ab48cc22fd95c60da3872aa236965
schipcar <cschippits@cs.uchicago.edu>
2022-05-15 15:40:57 -0500

    Modified assignments schema

--- create.sql
+++ create.sql
@@ -3,8 +3,7 @@ CREATE TABLE grades (id INTEGER PRIMARY KEY, student TEXT, name TEXT, status TEX
 CREATE TABLE users (name TEXT, email TEXT, id INTEGER, role TEXT check(role='student' or role='teacher' or role='admin'), status TEXT check(status='active' or status='inactive'), password TEXT, sec_q1 TEXT, sec_q2 TEXT, sec_q3 TEXT);
 CREATE TABLE courses (name TEXT, teacher TEXT, description TEXT, capacity INTEGER);
 CREATE TABLE courses_students (course_name TEXT, user_id INTEGER);
-CREATE TABLE courses_assignments (course_name TEXT, assignment_name TEXT);
-CREATE TABLE assignments (name TEXT, due_date TEXT, points INTEGER, description TEXT);
+CREATE TABLE assignments (assignment_name TEXT, course_name TEXT, due_date TEXT, points INTEGER, description TEXT);
 CREATE TABLE courses_announcements (course_name TEXT, announcement_subject TEXT);
 CREATE TABLE announcements (subject TEXT, body TEXT);
 
@@ -26,15 +25,10 @@ INSERT INTO courses_students VALUES ("Machine Learning", 0001);
 INSERT INTO courses_Students VALUES ("Machine Learning", 0002);
 INSERT INTO courses_students VALUES ("Algorithms", 0001);
 
-INSERT INTO courses_assignments VALUES ("Web Development", "Homework 1");
-INSERT INTO courses_assignments VALUES ("Web Development", "Homework 2");
-INSERT INTO courses_assignments VALUES ("Web Development", "Homework 3");
-INSERT INTO courses_assignments VALUES ("Web Development", "Homework 4");
-
-INSERT INTO assignments VALUES ("Homework 1", "04/20/2022", 100, "This is the description for HW1");
-INSERT INTO assignments VALUES ("Homework 2", "05/17/2022", 100, "This is the description for HW2");
-INSERT INTO assignments VALUES ("Homework 3", "05/20/2022", 100, "This is the description for HW3");
-INSERT INTO assignments VALUES ("Homework 4", "05/31/2022", 100, "This is the description for HW3");
+INSERT INTO assignments VALUES ("Homework 1", "Web Development", "04/20/2022", 100, "This is the description for HW1");
+INSERT INTO assignments VALUES ("Homework 2", "Web Development", "05/17/2022", 100, "This is the description for HW2");
+INSERT INTO assignments VALUES ("Homework 3", "Web Development", "05/20/2022", 100, "This is the description for HW3");
+INSERT INTO assignments VALUES ("Homework 4", "Web Development", "05/31/2022", 100, "This is the description for HW3");
 
 INSERT INTO announcements VALUES ("HW deadline pushed back", "HW3 will now be due on Friday. Good luck!");
 INSERT INTO announcements VALUES ("Pop quiz", "There will be a pop quiz in class tomorrow. Good luck!");
