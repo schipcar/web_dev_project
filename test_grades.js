9cdd600a174538aaf6f64c314d0788a965062456
anasofiamvaladez <anasofiamvaladez@gmail.com>
2022-05-18 12:36:17 -0500

    Update server after professor correction

--- test_grades.js
+++ test_grades.js
@@ -6,11 +6,12 @@ var http = require('http');
     myParser = require("body-parser");
     express = require("express");
     pag = require('https');
+    db = 0
 var row_sent = 2;
 
 
-
-let db = new sqlite3.Database('./canvas.db', (err) => {
+function renew() {
+  db = new sqlite3.Database('./canvas.db', (err) => {
   if (err) {
     console.error(err.message);
   } 
@@ -18,32 +19,25 @@ let db = new sqlite3.Database('./canvas.db', (err) => {
     row_sent=row
   });
 });
+}
+renew()
 
-
-
-http.createServer(function(req, response){
-  var path = url.parse(req.url).pathname;
-  if(path=="/getdictionary"){
-      response.setHeader('Access-Control-Allow-Origin', '*');
-      response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-      response.setHeader('Access-Control-Max-Age', 2592000);
-      response.setHeader('Content-Type', 'application/json');
-      const jsonContent = JSON.stringify(row_sent);
-      response.end(jsonContent);
-      console.log(jsonContent); 
-    }
-    }
-).listen(8080);
-
-console.log("server initialized");
-
-var app     = express();
-app.use(myParser.urlencoded({ extended: true })); 
+var app = express();
+app.use(myParser.urlencoded({ extended: true }));
+app.get("/getdictionary", function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Max-Age', 2592000);
+  res.setHeader('Content-Type', 'application/json');
+  renew()
+  res.send(JSON.stringify(row_sent)); 
+});
 app.post('/putdictionary', function(req, res) {
   db.run('INSERT INTO grades(id, student, name, status, punctuation, possible) VALUES(?,?,?,?,?,?)', [req.body.id, req.body.student, req.body.name, req.body.status, req.body.punctuation, req.body.possible]);
-  res.send("")
+  renew()
+  res.status(204).send()
 });
-app.listen(3000, function() {
+app.listen(8080, function() {
   console.log('Server running at http://127.0.0.1:3000/');
 });
 
