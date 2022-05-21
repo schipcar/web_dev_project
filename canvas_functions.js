2ed68bf2b8db471b6a52abf87d174622c12813af
Carly Schippits <77079862+schipcar@users.noreply.github.com>
2022-05-21 11:05:14 -0500

    Update canvas_functions.js

--- canvas_functions.js
+++ canvas_functions.js
@@ -471,7 +471,7 @@ function LoadAnnouncements() {
   course_name = data.course_name
   if (typeof window !== 'undefined') {
       localStorage.setItem("course_name", course_name)
-      document.getElementById("main_panel").inneHTML += localStorage.getItem("course_name")
+      document.getElementById("main_panel").inneHTML += "test" //localStorage.getItem("course_name")
   }
   
   let xhttp = new XMLHttpRequest();
