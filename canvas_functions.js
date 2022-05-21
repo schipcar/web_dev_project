65156c55e4640f24b1037bb48ee0014be01d0974
Carly Schippits <77079862+schipcar@users.noreply.github.com>
2022-05-21 11:54:52 -0500

    Update canvas_functions.js

--- canvas_functions.js
+++ canvas_functions.js
@@ -258,13 +258,6 @@ let db = new sqlite3.Database('./canvas.db', (err) => {
   db.all("SELECT * FROM courses WHERE teacher = (SELECT name FROM users WHERE id = ?)", [user], function(err, row) {
     courses_row_teacher=row
   });
-  console.log("test")
-  if (typeof window !== 'undefined') {
-    course_name = localStorage.getItem("course_name")
-  }
-  db.all("SELECT * FROM announcements WHERE subject IN (SELECT announcement_subject FROM courses_announcements WHERE course_name = ?)", [course_name], function(err, row) {
-    announcements_row=row
-  });
   user = 0001; /* REMOVE LATER */
   db.all("SELECT * FROM assignments WHERE course_name IN (SELECT course_name FROM courses_students WHERE user_id = ?)", [user], function(err, row) {
     all_assignments_row_student=row
@@ -470,10 +463,10 @@ function LoadAnnouncements() {
       data[tmp[0]] = decodeURI(tmp[1]);
   }
   course_name = data.course_name
-  if (typeof window !== 'undefined') {
-      localStorage.setItem("course_name", course_name)
-  }
-  console.log(localStorage.getItem("course_name"))
+  db.all("SELECT * FROM announcements WHERE subject IN (SELECT announcement_subject FROM courses_announcements WHERE course_name = ?)", [course_name], function(err, row) {
+    announcements_row=row
+  });
+  console.log(course_name)
   
   let xhttp = new XMLHttpRequest();
   xhttp.open("GET", "http://localhost:8070/getannouncements", true);
