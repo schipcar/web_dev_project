65eb57fe6bc7a727badc0a299d4c4d5f3140167f
anasofiamvaladez <anasofiamvaladez@gmail.com>
2022-05-23 17:00:45 -0500

    Restrict to student view and new schema changes

--- test_grades.js
+++ test_grades.js
@@ -8,7 +8,8 @@ var http = require('http');
     pag = require('https');
     db = 0
 var row_sent = 2;
-var proof = 0;
+var row_student = 0;
+var string_student = "SELECT * FROM grades";
 
 
 function renew() {
@@ -18,6 +19,10 @@ function renew() {
   } 
   db.all("SELECT * FROM grades", function(err, row) {
     row_sent=row
+  });
+  db.all(string_student, function(err, row) {
+    console.log(row)
+    row_student=row
   });
 });
 }
@@ -34,12 +39,29 @@ app.get("/getdictionary", function(req, res) {
   res.send(JSON.stringify(row_sent)); 
 });
 app.post('/putdictionary', function(req, res) {
-  db.run('INSERT INTO grades(id, student, name, status, punctuation, possible) VALUES(?,?,?,?,?,?)', [req.body.id, req.body.student, req.body.name, req.body.status, req.body.punctuation, req.body.possible]);
+  db.run('INSERT INTO grades(email, punctuation, possible, course_name) VALUES(?,?,?,?)', [req.body.email, req.body.punctuation, req.body.possible, req.body.course_name]);
   renew()
   res.status(204).send()
 });
+app.post('/gradestudent', function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+  res.setHeader('Access-Control-Max-Age', 2592000);
+  res.setHeader('Content-Type', 'application/json');
+  string_student = 'SELECT * FROM grades WHERE email=' + '"' + String(req.body.student) + '"'
+  renew()
+  res.status(204).send()
+});
+app.get('/gradestudent2', function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Max-Age', 2592000);
+  res.setHeader('Content-Type', 'application/json');
+  renew()
+  res.send(JSON.stringify(row_student)); 
+});
 app.listen(8080, function() {
-  console.log('Server running at http://127.0.0.1:3000/');
+  console.log('Server running at http://127.0.0.1:8080/');
 });
 
 
